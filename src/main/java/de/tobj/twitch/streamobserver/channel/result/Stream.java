39e396ea811268ae155060f96da9a15a16c26409
TobJ <code@tobj.de>
2015-08-14 18:28:14 +0200

    add new property to ignore

--- src/main/java/de/tobj/twitch/streamobserver/channel/result/Stream.java
+++ src/main/java/de/tobj/twitch/streamobserver/channel/result/Stream.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonIgnoreProperties(value = { "_links" })
+@JsonIgnoreProperties(value = { "_links", "is_playlist" })
 public class Stream {
 	@JsonProperty("game")
 	private String game;
